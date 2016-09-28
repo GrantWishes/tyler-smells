369d458416ae32f0c3018aaffe54434f90a583af
GrantWishes <grant-gertsen@uiowa.edu>
2016-09-27 20:26:53 -0500

    Redirection implemented. No comments yet so it's a hot mess, but I've been at the library for 5 hours and I'll do them later

--- shell.c
+++ shell.c
@@ -6,8 +6,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <fcntl.h>
 
 #define MAX_INPUT (513) // 512 characters + carriage return
+
+char *outFile = NULL;
 
 void error() {
 	/* The one true error message. */
@@ -25,60 +28,78 @@ char **parse(char arguments[], size_t size) {
 	
 	// TYLER'S GARBAGE CODE STARTS HERE
 
-	char argCopy[strlen(arguments)];
-	strcpy(argCopy, arguments);
-	char *myPtr = NULL; // the > substring we're looking for
-        bool redirect; // boolean to determine whether > is valid
-	myPtr = strstr(arguments, ">"); // is > present? Well if it is then gg
-	int count; // initialize a count variable, will be used later
-	bool arrowActive; // this boolean keeps track of when we hit a ">"
-        char *myPtr2 = NULL; // initialize a ptr
-	char *myPtr3 = NULL; // initialize another ptr
-	if (myPtr != NULL) {
-	  myPtr2 = strtok(arguments, ">"); // split the string
-	  // at the > marker
-	  count = 0; // Initialize count to be 0
-	  while (myPtr2 != NULL) { // while the second pointer has
-	    // a value, keep incrementing count
-	    count++ ;
-	    myPtr2 = strtok(NULL, ">");
-	  }
-	  if (count == 2) { // At this point, count needs to equal
-	    // 2 otherwise there were too many ">" marks
-	    myPtr3 = strtok(argCopy, " "); // split the string at spaces
-	    arrowActive = false; //arrowActive boolean is currently set to false
-	    count = 0;
-	    while (myPtr3 != NULL) {
-	      // if the arrow has already been passed then we need to
-	      // increment count to keep track of args after it
-	      if (arrowActive) {
-		count++;
-	      }
-	      // active the arrow boolean if we have encountered the arrow in
-	      // the line
-	      if (strcmp(myPtr3, ">") == 0) {
-		arrowActive = true;
-	      }
-	      myPtr3 = strtok(NULL, " ");
-	    }
-	    // if there was only one argument after it, then the Redirection
-	    // command is valid
-	    if (count == 1) {
-	      redirect = true;
-	      printf("Redirection flag raised\n");
-	    }
-	    else {
-	      redirect = false;
-	    }
-	  }
-	}
+//	char argCopy[strlen(arguments)];
+//	strcpy(argCopy, arguments);
+//	char *myPtr = NULL; // the > substring we're looking for
+//        bool redirect; // boolean to determine whether > is valid
+//	myPtr = strstr(arguments, ">"); // is > present? Well if it is then gg
+//	int count; // initialize a count variable, will be used later
+//	bool arrowActive; // this boolean keeps track of when we hit a ">"
+  //      char *myPtr2 = NULL; // initialize a ptr
+//	char *myPtr3 = NULL; // initialize another ptr
+//	if (myPtr != NULL) {
+//	  myPtr2 = strtok(argCopy, ">"); // split the string
+//	  // at the > marker
+//	  count = 0; // Initialize count to be 0
+//	  while (myPtr2 != NULL) { // while the second pointer has
+//	    // a value, keep incrementing count
+//	    count++ ;
+//	    myPtr2 = strtok(NULL, ">");
+//	  }
+//	  if (count == 2) { // At this point, count needs to equal
+//	    // 2 otherwise there were too many ">" marks
+//	    strcpy(argCopy,arguments);
+//	    myPtr3 = strtok(argCopy, " "); // split the string at spaces
+//	    arrowActive = false; //arrowActive boolean is currently set to false
+//	    count = 0;
+//	    while (myPtr3 != NULL) {
+//	      // if the arrow has already been passed then we need to
+//	      // increment count to keep track of args after it
+//	      if (arrowActive) {
+//		count++;
+//	      }
+//	      // active the arrow boolean if we have encountered the arrow in
+//	      // the line
+//	      if (strcmp(myPtr3, ">") == 0) {
+//		arrowActive = true;
+//	      }
+//	      myPtr3 = strtok(NULL, " ");
+//	    }
+//	    // if there was only one argument after it, then the Redirection
+//	    // command is valid
+//	    if (count == 1) {
+//	      redirect = true;
+//	      printf("Redirection flag raised\n");
+//	    }
+//	    else {
+//	      redirect = false;
+//	    }
+//	  }
+//	}
 
 	// END TYLER'S GARBAGE CODE
+	// grant's garbo code here
+	
+	bool redirect = false;
+	if(strstr(arguments,">") != NULL) {
+		redirect = true;
+//		int counter = 0;
+//		for(int i = 0; i<strlen(arguments); i++){
+//			if(arguments[i] == '>') {
+//				counter++;
+//			}
+//		}
+//		if(counter == 1) {
+//			redirect = true;
+//			printf("Yeah, this works\n");
+//		}
+	}		
 
+	// grant's garbo ends here
 	
 	
 	/* pointer to each individual word */
-	ptr = strtok(arguments, " ");
+	ptr = strtok(arguments, "> ");
 	
 	int i = 0;		//this variable taught me about undefined behavior
 	char* pyPtr;		//the substring we're looking for
@@ -101,10 +122,18 @@ char **parse(char arguments[], size_t size) {
 	}
 	/* adding the words to the array */
 	
-	
 	for (i; ptr != NULL; i++) {
+//		printf("Adding \"%s\" to index %d\n", ptr, i);
 		words[i] = ptr;
-		ptr = strtok(NULL, " ");
+		ptr = strtok(NULL, "> ");
+		if(ptr == NULL) {
+			if(redirect == true) {
+				//printf("Output file is %s\n", words[i]);
+				outFile = strdup(words[i]);
+				words[i] = '\0';
+				break;
+			}
+		}
 	}
 
 	//	Debug print
@@ -121,7 +150,9 @@ int main(int argc, char *argv[]) {
 	char **args;		// for the command and arguments parsed
 	char pwd[MAX_INPUT*2];	// for the pwd functionality, size seemed common
 
-	
+	for(int i = 0; i < argc; i++) {
+		printf("Argument: %s\n", argv[i]);
+	}
 
 	while(1) {
 		/* The shell prompt */	
@@ -147,6 +178,8 @@ int main(int argc, char *argv[]) {
 		/* get our arguments in an array form */
 		args = parse(input,sizeof(input));
 
+		printf("Output file is %s\n", outFile);
+
 		/* Exit command */	
 		if(strcmp(args[0],"exit") == 0) {
 			exit(0);
@@ -155,7 +188,21 @@ int main(int argc, char *argv[]) {
 		/* print working directory command */
 		else if(strcmp(args[0],"pwd")==0) {
 			if(getcwd(pwd,sizeof(pwd)) != NULL) {
+				if(outFile != NULL) {
+					int out = dup(1);
+					close(STDOUT_FILENO);
+					int file = open(outFile, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
+					if(file < 0) {
+						error();
+						continue;
+					}	
+					printf("%s\n",pwd);
+					close(file);
+					dup2(out, STDOUT_FILENO);
+				}
+				else {
 				printf("%s\n", pwd);
+				}
 			}
 			else {
 				error();
@@ -184,12 +231,27 @@ int main(int argc, char *argv[]) {
 			pid_t pid = fork();   //i accidentally fork bombed here
 			int status;
 			
-			if(pid < 0) {
+			if(pid < (pid_t) 0) {
 				error();
 			}
 			/* The child process runs the command */
-			else if(pid == 0) {
-				if(execvp(args[0],args) < 0) {
+			else if(pid ==  0) {
+				if(outFile != NULL) {
+					close(1);
+					int fd = open(outFile, O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
+					if(fd < 0) {
+						error();
+					}
+
+					if(execvp(args[0],args) < 0) {
+						error();
+						exit(1);	
+					}
+				}
+
+
+
+				else if(execvp(args[0],args) < 0) {
 					error();
 					exit(1);
 				}
@@ -203,7 +265,8 @@ int main(int argc, char *argv[]) {
 
 
 		/* clears the array for the next batch of arguments */
-		args[0] = '\0';			
+		args[0] = '\0';		
+		outFile = NULL;	
 	}
 
 	return 0;
